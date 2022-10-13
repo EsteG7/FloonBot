067ec2cccb23cbc35bc7105723293dabbb4d2bdc
floonbot <floon.wagon@gmail.com>
2022-10-13 21:13:30 +0200

    add msj

--- Loaders/loadDatabase.js
+++ Loaders/loadDatabase.js
@@ -3,9 +3,11 @@ const mysql = require("mysql")
 module.exports = async () => {
 
   let db = await mysql.createConnection({
+
     host: "localhost",
     user: "root",
     password: "",
+    //Mettre le nom de ta database
     database: "floon"
 
   })
